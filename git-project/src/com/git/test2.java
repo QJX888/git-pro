be671e90073bdd6b1202fca890e2154d8b4f6ae8
QJX888 <1841907617@qq.com>
2020-06-24 10:59:25 +0800

    修改

--- git-project/src/com/git/test2.java
+++ git-project/src/com/git/test2.java
@@ -1,9 +1,20 @@
 package com.git;
 
+
+
+
 public class test2 {
 	private int id;
 	
+	private int age;
 	
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 	public int getId() {
 		
 		return id +10;
